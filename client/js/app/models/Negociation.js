b981f01d908215791b1bfaff1cf7b5f8aec48e97
remonts <monteiro-renan@hotmail.com>
2021-07-21 19:46:34 -0300

    Protected trading class variables against undue changes.

--- client/js/app/models/Negociation.js
+++ client/js/app/models/Negociation.js
@@ -1,5 +1,24 @@
 class Negociation {
-    constructor() {
-        
+    constructor(date, amount, value) {
+        this._date = new Date(date.getTime());
+        this._amount = amount;
+        this._value = value;
+        Object.freeze(this);
+    }
+
+    get date() {
+        return new Date(this._date.getTime());
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    get size() {
+        return this._amount * this._value;
+    }
+
+    get value() {
+        return this._value;
     }
 }
